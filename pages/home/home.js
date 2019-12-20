83d798ef4aa7013f45e6e9398fd7f1b1de9902f3
Cpy <617476825@qq.com>
2019-12-20 15:21:50 +0800

    知识点03

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,30 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'CPY',
+    age: 18,
+    students:[
+      {id:10,name:'kobe',age:20},
+      {id:20,name:'why',age:90},
+      {id:30,name:'james',age:25},
+      {id:40,name:'curry',age:60}
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //设置监听事件 + 
+  counterAdd(){
+    //this指的是Page这个对象
+    // this.data.counter+=1;
+    // console.log(this.data.counter);
+    //上面这个方法确实改变了变量，但是不会在页面上刷新
+    this.setData({
+      //通过setData这个方法，可以刷新页面的数据
+      counter:this.data.counter+1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //设置监听事件 -
+  counterDel(){
+    this.setData({
+      counter:this.data.counter-1
+    })
   }
 })
