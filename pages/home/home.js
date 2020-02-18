af54bc327b7b74267b04ffaa5d77befad29fc463
Cpy <617476825@qq.com>
2020-02-18 17:18:01 +0800

    10_小程序参数传递、捕获、冒泡

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,71 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    titles:['衣服','裤子','鞋子']
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 触摸开始事件
+  handleTouchStart(){
+    console.log('触摸开始')
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 触摸后移动
+  handleTouchMove(){
+    console.log('触摸后移动') 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 触摸被打断
+  handleTouchCancel(){
+    console.log('触摸被打断')
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 触摸结束
+  handleTouchEnd(){
+    console.log('触摸结束')
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  //触摸后马上离开(不超过350ms,例如点击)
+  handleTap(){
+    console.log('触摸后马上离开')
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  // 触摸超过350ms触发回调函数，tap事件不会触发(长按)
+  handleLongPress(){
+    console.log('触摸超过350ms触发回调函数，tap事件不会触发')
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  handleEventClick(event){
+    console.log('----',event)
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleTouchEnd2(event){
+    console.log('++++',event)
+  },
+  handleTouchStart2(event){
+    console.log('``````',event)
+  },
+  handleOuterClick(event){
+    console.log('handleOuterClick',event)
+  },
+  handleInnerClick(event){
+    console.log('handleInnerClick',event)
+  },
+  handleItemClick(event){
+    console.log(event);
+    const dataset = event.currentTarget.dataset;
+    const index = dataset.index;
+    const item = dataset.item;
+    console.log(index,item);
+  },
+  handleCaptrueView1(){
+    console.log('handleCaptrueView1')
+  },
+  handleBindView1(){
+    console.log('handleBindView1')
+  },
+  handleCaptrueView2() {
+    console.log('handleCaptrueView2')
+  },
+  handleBindView2() {
+    console.log('handleBindView2')
+  },
+  handleCaptrueView3() {
+    console.log('handleCaptrueView3')
+  },
+  handleBindView3() {
+    console.log('handleBindView3')
   }
 })
+
